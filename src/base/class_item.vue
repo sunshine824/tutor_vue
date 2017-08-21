f470b228236111d8eac5beb5a2aa5bb70e56c64e
Administrator <790168710@qq.com>
2017-08-21 18:26:49 +0800

    1.0

---
+++ src/base/class_item.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="class_item clearfix">
+    <div class="class_desc">
+      <p class="class_txt">课程24：使用哑铃锻炼肩袖肌群</p>
+      <div class="btns">
+        <span class="class_name">哑铃训练</span>
+        <span class="class_money">免费</span>
+      </div>
+    </div>
+    <div class="class_img">
+      <img src="../assets/class_img.png"/>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {}
+    }
+  }
+</script>
+<style scoped lang="less">
+  .class_item{
+    background: #fff;
+    margin-top: 5px;
+    padding: .2rem .3rem .5rem .5rem;
+  }
+  .class_desc {
+    width: 55%;
+    float: left;
+    .class_txt{
+      font-size: .4rem;
+      font-weight: 600;
+      color: #5b5b5b;
+      margin-bottom: .6rem;
+    }
+    .btns{
+      span{
+        text-align: center;
+        float: left;
+        height: .9rem;
+        line-height: .9rem;
+        font-size: .4rem;
+        border-radius: .25rem;
+        &.class_name{
+          color: #5b5b5b;
+          padding: 0 .4rem;
+          border:1px solid #5b5b5b;
+          margin-right: .2rem;
+        }
+        &.class_money{
+          color: #fff;
+          background: #1eacc7;
+          width: 2rem;
+        }
+      }
+    }
+  }
+  .class_img {
+    width: 40%;
+    float: right;
+    margin-top: .2rem;
+    img {
+      width: 100%;
+      height: 2.5rem;
+    }
+  }
+</style>
---
+++ src/components/index.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="index_class">
+    <div class="banner">
+      <img src="../assets/banner.png" alt="" class="bg_img"/>
+      <a href="javascript:;" class="enter"></a>
+      <a href="javascript:;" class="wechat"></a>
+    </div>
+    <div class="classes clearfix">
+      <ul class="clearfix">
+        <li v-for="(item,index) in classes" :key="index">
+          <a href="javascript:;">{{item}}</a>
+        </li>
+      </ul>
+    </div>
+    <class-item></class-item>
+    <class-item></class-item>
+    <class-item></class-item>
+    <class-item></class-item>
+    <class-item></class-item>
+    <class-item></class-item>
+  </div>
+</template>
+<script>
+  import classItem from '../base/class_item'
+
+  export default {
+    components: {
+      classItem
+    },
+    data() {
+      return {
+        classes: ['全部课程', '哑铃训练', '杠铃训练']
+      }
+    }
+  }
+</script>
+<style scoped lang="less">
+  .banner {
+    width: 100%;
+    height: auto;
+    overflow: auto;
+    display: flex;
+    position: relative;
+    .bg_img {
+      width: 100%;
+      height: 100%;
+    }
+    a {
+      position: absolute;
+      &.enter {
+        width: 3rem;
+        height: .9rem;
+        top: 17%;
+        right: 7%;
+      }
+      &.wechat {
+        width: 3rem;
+        height: 1.2rem;
+        top: 50%;
+        right: 7%;
+      }
+    }
+  }
+
+  .classes {
+    background: #fff;
+    ul {
+      li {
+        float: left;
+        width: 33.333333%;
+        font-size: .45rem;
+        text-align: center;
+        padding: .2rem 0 0 0;
+        a {
+          color: #5b5b5b;
+          border-bottom: 5px solid #1eacc7;
+          padding-bottom: .2rem;
+          display: inline-block;
+        }
+      }
+    }
+  }
+</style>
