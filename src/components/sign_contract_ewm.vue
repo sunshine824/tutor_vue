12ac3099b0b232bcba39616abc0d656692c5a866
Administrator <790168710@qq.com>
2017-08-22 12:45:20 +0800

    1.0

--- src/components/sign_contract_ewm.vue
+++ src/components/sign_contract_ewm.vue
@@ -1,7 +1,14 @@
 <template>
-  <Bg>
-    <h1>导师二维码页面</h1>
-  </Bg>
+  <div class="ewm">
+    <Bg>
+      <p class="end_title">最后一步</p>
+      <p class="end_title1">人工认证：添加微信好友</p>
+      <div class="erweima">
+        <img :src="erweima"/>
+      </div>
+      <p class="hint">长按上图二维码，添加微信好友</p>
+    </Bg>
+  </div>
 </template>
 <script type="text/ecmascript-6">
   import Bg from '../base/bg'
@@ -11,10 +18,33 @@
       Bg
     },
     data() {
-      return {}
+      return {
+        erweima:'../static/images/erweima_03.png'
+      }
     }
   }
 </script>
 <style scoped lang="less">
-
+  .ewm{
+    p{
+      text-align: center;
+      font-size: .45rem;
+      color: #505050;
+      &.end_title{
+        margin-top: 22%;
+      }
+      &.end_title1{
+        margin-top: 1%;
+      }
+    }
+    .erweima{
+      width: 75%;
+      margin: 5% auto 3% auto;
+      display: block;
+      img{
+        width:100%;
+        display: block;
+      }
+    }
+  }
 </style>
